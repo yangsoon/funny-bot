66e6edd166816898ad0903dcf788576646ac2307
yangsoon <yangsoon@aliyun.com>
2017-12-06 16:26:13 +0800

    base function

---
+++ main.py
@@ -0,0 +1,42 @@
+# -*- coding : utf-8 -*-
+import telegram
+import json
+import logging
+from commands import start, remove, get_list, \
+    change_page, get_photo, FilterTypes, FilterKey
+from telegram.ext import CommandHandler, CallbackQueryHandler, \
+    Updater, MessageHandler
+
+with (open('token.conf', 'r')) as f:
+    token = json.loads(f.read())['token']
+
+
+def error_callback(bot, update, error):
+    raise error
+
+
+class FunnyPhoto:
+
+    def __init__(self):
+        logging.basicConfig(
+            format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+        self.updater = Updater(token=token)
+        self.dispatcher = self.updater.dispatcher
+        self.begin()
+
+    def begin(self):
+        filter_types = FilterTypes()
+        filter_key = FilterKey()
+        start_handler = CommandHandler('start', start)
+        types_handler = MessageHandler(filter_types, get_list)
+        key_handler = MessageHandler(filter_key, get_photo)
+        self.dispatcher.add_handler(start_handler)
+        self.dispatcher.add_handler(CallbackQueryHandler(change_page))
+        self.dispatcher.add_handler(types_handler)
+        self.dispatcher.add_handler(key_handler)
+        self.dispatcher.add_handler(CommandHandler('remove', remove))
+        self.dispatcher.add_error_handler(error_callback)
+
+
+if __name__ == "__main__":
+    bot = FunnyPhoto()
---
+++ spider.py
@@ -0,0 +1,56 @@
+# -*- coding : utf-8 -*-
+import re
+import requests
+import tornado
+from bs4 import BeautifulSoup
+
+root_url = 'http://www.gamersky.com'
+
+
+def fetch_list(mark=None, page=None):
+    if not mark:
+        target_url = root_url + '/ent/111/'
+    else:
+        target_url = root_url + '/ent/' + mark + '/'
+    if page:
+        target_url = target_url + page
+    print("fecth: {}".format(target_url))
+    response = requests.get(target_url).text
+    content = BeautifulSoup(response, 'html.parser')
+    target_list = content.find(class_="pictxt").find_all("li")
+    results = [{
+        'thumbnail': item.img['src'],
+        'title': item.a['title'],
+        'link': item.a['href']
+    } for item in target_list]
+    try:
+        pre = content.find(class_='p1 previous')['href']
+    except TypeError:
+        pre = None
+    try:
+        nexe = content.find(class_='p1 nexe')['href']
+    except TypeError:
+        nexe = None
+    return results, pre, nexe
+
+
+def fetch_photo(key, page=None):
+    target_url = root_url + '/ent/' + key
+    if page:
+        target_url = target_url + '_' + str(page) + '.shtml'
+    else:
+        target_url += '.shtml'
+    print(target_url)
+    response = requests.get(target_url)
+    response.encoding = 'utf-8'
+    response = response.text
+    patt_next = re.compile(u'<a href="http://www.gamersky.com/ent/' + key + '_(?P<page>\d+)\.shtml">下', re.S)
+    try:
+        nexe = patt_next.search(response).group('page')
+    except AttributeError:
+        nexe = None
+    patt_img = re.compile('<p.*?center">.*?<img.*?src="(.*?)".*?>', re.S)
+    results = re.findall(patt_img, response)
+    if len(results) == 0:
+        print("fecth error from url: {}".format(target_url))
+    return results, nexe
