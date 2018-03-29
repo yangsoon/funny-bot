04d81952744b0c6cb56f549f25c7ed37571eff96
yangsoon <yangsoon@aliyun.com>
2018-03-29 21:46:35 +0800

    rebuild code with asyncio

--- main.py
+++ main.py
@@ -1,44 +1,13 @@
-# -*- coding : utf-8 -*-
-import telegram
 import json
-import logging
-from commands import start, remove, get_list, \
-    change_page, get_photo, FilterTypes, FilterKey
-from telegram.ext import CommandHandler, CallbackQueryHandler, \
-    Updater, MessageHandler
+from aiotg import Bot, Chat
 
-with (open('token.conf', 'r')) as f:
-    token = json.loads(f.read())['token']
+with open("token.conf") as f:
+    conf = json.load(f)
 
+bot = Bot(**conf)
 
-def error_callback(bot, update, error):
-    raise error
+@bot.command(r"/echo (.+)")
+def echo(chat: Chat, match):
+    return chat.reply(match.group(1))
 
-
-class FunnyPhoto:
-
-    def __init__(self):
-        logging.basicConfig(
-            format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
-        self.updater = Updater(token=token)
-        self.dispatcher = self.updater.dispatcher
-        self.begin()
-
-    def begin(self):
-        filter_types = FilterTypes()
-        filter_key = FilterKey()
-        start_handler = CommandHandler('start', start)
-        types_handler = MessageHandler(filter_types, get_list)
-        key_handler = MessageHandler(filter_key, get_photo)
-        self.dispatcher.add_handler(start_handler)
-        self.dispatcher.add_handler(CallbackQueryHandler(change_page))
-        self.dispatcher.add_handler(types_handler)
-        self.dispatcher.add_handler(key_handler)
-        self.dispatcher.add_handler(CommandHandler('remove', remove))
-        self.dispatcher.add_error_handler(error_callback)
-
-
-if __name__ == "__main__":
-    bot = FunnyPhoto()
-    bot.updater.start_polling()
-    bot.updater.idle()
+bot.run()
